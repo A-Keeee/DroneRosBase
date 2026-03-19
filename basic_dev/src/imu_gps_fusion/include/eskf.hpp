3e81380b6fb6b2ce3aa2096b26e838ecb811cba8
zyicome <573332957@qq.com>
2026-03-19 19:03:49 +0800

    init

--- basic_dev/src/imu_gps_fusion/include/eskf.hpp
+++ basic_dev/src/imu_gps_fusion/include/eskf.hpp
@@ -17,11 +17,11 @@ private:
     // State vector: [position(3), velocity(3), orientation(3), gyro_bias(3), accel_bias(3)]
     Eigen::Matrix<double, 15, 15> P; // Error covariance matrix
     Eigen::Matrix<double, 15, 15> F_x; // State transition matrix
-    Eigen::Matrix<double, 
+    Eigen::Matrix<double, 15, 15> F_u; // Control input matrix
     Eigen::Matrix<double, 15, 15> Q; // Process noise covariance matrix
     Eigen::Matrix<double, 6, 6> R; // Measurement noise covariance matrix
-    Eigen::Vector<double, 15> x; // State vector
-    Eigen::Vector<double, 6> measurement; // Measurement vector
+    Eigen::Matrix<double, 15, 1> x; // State vector
+    Eigen::Matrix<double, 6, 1> measurement; // Measurement vector
 };
 
 #endif // _ESKF_HPP_
